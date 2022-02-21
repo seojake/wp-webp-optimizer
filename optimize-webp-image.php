b7876cf373cfcc0811d401ce25fb1c7735ec24a5
Jake Punton <jake@seojake.com>
2022-02-21 21:28:41 +0000

    added function to be called within theme that will find all images that haven't yet been optimized and run the optimizer

--- optimize-webp-image.php
+++ optimize-webp-image.php
@@ -2,7 +2,9 @@
 
 /**
  * Plugin Name: Optimize WebP Images
+ * Description: Convert PNGs and JPEGs to WebP for image optimization.
  * Author: seojake
+ * Author URI: https://github.com/seojake
  * Version: 1.0.0
  */
 
@@ -12,11 +14,31 @@ require_once 'OptimizeWebP.php';
 add_filter('big_image_size_threshold', '__return_false');
 
 // When a file is being uploaded, automatically optimize it
-add_action('add_attachment', 'owebp_optimize_uploaded_image', 10, 1);
+// add_action('add_attachment', 'optimizewebp__uploaded_image', 10, 1);
 function owebp_optimize_uploaded_image($id)
 {
   $optimizer = new OptimizeWebP($id);
   $optimizer->optimize();
 }
 
-// Add a admin page to the tools page
+// Optimize all images that haven't been already
+function optimizewebp_all()
+{
+  $images = new WP_Query([
+    'post_type' => 'attachment',
+    'post_status' => 'inherit',
+    'post_mime_type' => [
+      'image/png',
+      'image/jpg',
+      'image/jpeg'
+    ],
+    'posts_per_page' => -1
+  ]);
+
+  if (!empty($images->posts)) {
+    foreach ($images->posts as $image) {
+      $optimizer = new OptimizeWebP($image->ID);
+      $optimizer->optimize();
+    }
+  }
+}
